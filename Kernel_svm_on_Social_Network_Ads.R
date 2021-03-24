4dac369a2320757c833e32efe56c262914e60f9d
Ashish Kumar <14398ashish@gmail.com>
2021-03-24 18:53:58 +0530

    Adds SVM model on R language

---
+++ Kernel_svm_on_Social_Network_Ads.R
@@ -0,0 +1,72 @@
+#Kernel SVM
+#importing the libraries
+library("tidyverse")
+library(ggplot)
+#Importing the dataset
+dataset = read_csv("Social_Network_Ads.csv")
+dataset = dataset[3:5]
+
+#Encoding the target feature as factor
+dataset$Purchased = factor(dataset$Purchased,labels = c(0,1))
+
+#Visualizing the dataset 
+#scatter plot
+ggplot(data=dataset,aes(x=Age,y=EstimatedSalary,
+                        colour=Purchased)) + 
+  geom_point()
+
+
+# Splitting the dataset into the Training set and Test set
+# install.packages('caTools')
+library(caTools)
+set.seed(123)
+split= sample.split(dataset$Purchased,SplitRatio = 0.75)
+training_set = subset(dataset,split==TRUE)
+test_set = subset(dataset,split=FALSE)
+
+#Feature Scaling 
+training_set[-3] = scale(training_set[-3])
+test_set[-3]=scale(test_set[-3])
+
+# Fitting Kernel SVM to the Training set
+# install.packages('e1071')
+library(e1071)
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'radial')
+
+##Predicting the test set result
+y_pred= predict(classifier,newdata = test_set[-3])
+
+
+# Visualising the Training set results
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3],
+     main = 'Kernel SVM (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set)
+plot(set[, -3], main = 'Kernel SVM (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
